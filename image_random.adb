72ca3dde7b4a36830d31e049c6a741857fa647dd
Jeffrey R. Carter <16976909+jrcarter@users.noreply.github.com>
2025-05-26 15:54:28 +0200

    Initial Skein version

--- image_random.adb
+++ image_random.adb
@@ -1,20 +1,22 @@
 -- Image_Random: True random numbers from a digital camera
--- Copyright (C) 2020 by Pragmada Software Engineering
+-- Copyright (C) by Pragmada Software Engineering
 -- Released under the terms of the BSD 3-Clause license; see https://opensource.org/licenses
 --
+-- 2025-06-01 Use Skein-1024 to allow variable result bytes
 -- 2020-09-01 Initial version
 --
 with Ada.Directories;
 with Ada.Sequential_IO;
+with Ada.Unchecked_Conversion;
 with GNAT.OS_Lib;
-with GNAT.SHA512;
+with PragmARC.Skein;
 
 package body Image_Random is
-   function Random return Ada.Streams.Stream_Element_Array is
+   function Random (Num_Bytes : in Positive := 128) return Ada.Streams.Stream_Element_Array is
       File_Name : constant String := "random.png";
 
-      Arg : GNAT.OS_Lib.Argument_List_Access
-         := GNAT.OS_Lib.Argument_String_To_List ("/usr/bin/fswebcam -q --no-banner --png 0 " & File_Name);
+      Arg : GNAT.OS_Lib.Argument_List_Access :=
+         GNAT.OS_Lib.Argument_String_To_List ("/usr/bin/fswebcam -q --no-banner --png 0 " & File_Name);
       Success : Boolean;
    begin -- Random
       GNAT.OS_Lib.Spawn (Program_Name => Arg (Arg'First).all, Args => Arg (Arg'First + 1 .. Arg'Last), Success => Success);
@@ -25,12 +27,17 @@ package body Image_Random is
       end if;
 
       Read_File : declare
-         File_Size : constant Ada.Streams.Stream_Element_Count :=
-            Ada.Streams.Stream_Element_Count (Ada.Directories.Size (File_Name) );
+         File_Size : constant Natural := Integer (Ada.Directories.Size (File_Name) );
 
-         subtype Buffer_List is Ada.Streams.Stream_Element_Array (1 .. File_Size);
+         subtype Buffer_List is PragmARC.Skein.Byte_List (1 .. File_Size);
+         --  subtype Buffer_List is Ada.Streams.Stream_Element_Array (1 .. Ada.Streams.Stream_Element_Offset (File_Size) );
 
          package Buffer_IO is new Ada.Sequential_IO (Element_Type => Buffer_List);
+
+         subtype Result_List is PragmARC.Skein.Byte_List (1 .. Num_Bytes);
+         subtype Stream_List is Ada.Streams.Stream_Element_Array (1 .. Ada.Streams.Stream_Element_Offset (Num_Bytes) );
+
+         function To_Stream is new Ada.Unchecked_Conversion (Source => Result_List, Target => Stream_List);
 
          File   : Buffer_IO.File_Type;
          Buffer : Buffer_List;
@@ -39,7 +46,7 @@ package body Image_Random is
          Buffer_IO.Read (File => File, Item => Buffer);
          Buffer_IO.Close (File => File);
 
-         return GNAT.SHA512.Digest (Buffer);
+         return To_Stream (PragmARC.Skein.Hash (Buffer, Num_Bytes => Num_Bytes, Block_Size => PragmARC.Skein.Size_1024) );
       end Read_File;
    end Random;
 end Image_Random;
--- image_random_test.adb
+++ image_random_test.adb
@@ -1,17 +1,31 @@
 -- Image_Random_Test: Test of Image_Random
--- Copyright (C) 2020 by Pragmada Software Engineering
+-- Copyright (C) by Pragmada Software Engineering
 -- Released under the terms of the BSD 3-Clause license; see https://opensource.org/licenses
 --
+-- 2025-06-01 Selectable number of bytes
 -- 2020-09-01 Initial version
 --
+with Ada.Command_Line;
 with Ada.Streams;
 with Ada.Text_IO;
 with Image_Random;
 
 procedure Image_Random_Test is
-   Rand : constant Ada.Streams.Stream_Element_Array := Image_Random.Random;
+   Num_Bytes : constant Positive := (if Ada.Command_Line.Argument_Count > 0 then Integer'Value (Ada.Command_Line.Argument (1) )
+                                     else 128);
+   Rand  : constant Ada.Streams.Stream_Element_Array := Image_Random.Random (Num_Bytes);
+
+   use type Ada.Streams.Stream_Element_Offset;
 begin -- Image_Random_Test
    Put : for I in Rand'Range loop
-      Ada.Text_IO.Put_Line (Item => Ada.Streams.Stream_Element'Image (Rand (I) ) );
+      Ada.Text_IO.Put (Item => Rand (I)'Image);
+
+      if I rem 10 = 0 then
+         Ada.Text_IO.New_Line;
+      end if;
    end loop Put;
+
+   if Rand'Last rem 10 /= 0 then
+      Ada.Text_IO.New_Line;
+   end if;
 end Image_Random_Test;
